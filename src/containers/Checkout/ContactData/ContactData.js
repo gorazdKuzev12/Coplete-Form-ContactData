e53fd150bda50d52597e02542de161ace85e3ba2
gorazdKuzev12 <gorazd.kuzev10@gmail.com>
2021-02-20 14:10:45 +0100

    Complete ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,27 +1,98 @@
 
-import React, {Component} from 'react';
-import Button  from '../../../components/UI/Button/Button'
+import React, { Component } from 'react';
+import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
-class ContactData extends Component
-{ 
-    state ={
-        name: '',
-        email: '',
-        address:{
-            street:'',
-            postalCode:''}
+import Input from '../../../components/UI/Input/Input'
+class ContactData extends Component {
+    state = {
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your ZipCode'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' }]
+                },
+                value: ''
+            }
+        },
+        loading: false
     }
 
-    render(){
-        return(
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm })
+    }
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+        return (
             <div className={classes.ContactData}>
                 <h4>Enter yout contact data</h4>
                 <form>
-                    <input type="text" name="name" placeholder="Your Name"/>
-                    <input type="email" name="email" placeholder="Your Email"/>
-                    <input type="text" name="street" placeholder="Your stret"/>
-                    <input type="text" name="postal" placeholder="Postal Code"/>
-              <Button btnType="Succes">Order</Button>
+                    {formElementsArray.map(formElement => (
+                        <Input
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                        />
+                    ))}
+                    <Button btnType="Succes">Order</Button>
                 </form>
             </div>
         );
